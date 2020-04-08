4e1b4215b88608cc91c0c4d8e4458ca3c661ea28
Dominim Vllasa <dominim.vllasa@outlook.de>
2020-04-08 15:15:54 +0200

    - fixed remove-function and take out movment of polygone

--- edit_gen_polygon.py
+++ edit_gen_polygon.py
@@ -1,20 +1,22 @@
 import sys
 
 from PySide2 import QtWidgets
-from PySide2.QtWidgets import QMainWindow
 import widget_polygone_view
 
 
-class MainWindow(QMainWindow):
+class PolygonEditor(QtWidgets.QWidget):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
+        super(PolygonEditor, self).__init__(parent)
+        print('EDITOR', widget_polygone_view.imagePolygon)
+
         w = widget_polygone_view.WidgetWindow(self)
         w.show()
 
 
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    m = MainWindow()
+    m = PolygonEditor()
     # w = WidgetWindow(m)
     m.resize(1000, 600)
     m.show()
